3bff55e71df92544aa7f9f65a05cc646bc3068be
jmonto55 <jmonto55@gmail.com>
2023-02-21 17:31:06 -0500

    Add navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
+import styles from '../sass/Navbar.module.scss';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/calculator">Calculator</Link>
-      </li>
-      <li>
-        <Link to="/quote">Quote</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const linkStyles = {
+    color: '#e7e7e7',
+    textDecoration: 'none',
+    fontSize: '1.2rem',
+  };
+  return (
+
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar_list}>
+        <li>
+          <Link style={linkStyles} to="/">Home</Link>
+        </li>
+        <li>
+          <Link style={linkStyles} to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link style={linkStyles} to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
