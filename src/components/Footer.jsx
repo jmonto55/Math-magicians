68e0c3f1fe3896d049dfc7d604fe94770b353fbc
jmonto55 <jmonto55@gmail.com>
2023-02-22 01:49:11 -0500

    Minor styles changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../sass/Footer.module.scss';
 const Footer = () => (
   <footer className={styles.footer}>
     <p>© 2023 Jose Montoya. All rights reserved.</p>
-    <p>Built with ❤️ in Medellin, CO</p>
+    <p>Built with 💙 in Medellin, CO</p>
   </footer>
 );
 
